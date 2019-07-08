a592d3a459a1ebff2fbbdc057220aa9f51e60700
sesan <sesan2007uk@gmail.com>
2019-07-08 23:24:11 +0100

    Created a map fuction

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankensten",
+          id: "asr1"
+        },
+        {
+          name: "James Bond",
+          id: "asr2"
+        },
+        {
+          name: "Bat Man",
+          id: "asr3"
+        }
+      ]
+    }
+  }
+
+  render() {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacts
-        </a>
-      </header>
-    </div>
-  );
+      {
+        this.state.monsters.map(monster => 
+          <h1 key = {monster.id}>{monster.name}</h1>
+        )  
+      }
+  </div>
+    )
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn Reacts
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
